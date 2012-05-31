054f9f857076429130b5e830238ac84fd0b2905b
Marcus Olsson <f0x3n@live.se>
2012-05-31 15:42:46 +0200

    Changed javascript

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,13 @@ function bbcode_ins(fieldId, tag)
 		if (document.selection)
 		{
 			field.focus();
-			sel = document.selection.createRange();
-			sel.text = '[' + tag + ']'+sel.text+'[/' + tag+']';
+			var selected = document.selection.createRange().text;
+			var ins = '[' + tag + ']' + selected + '[/' + tag +']';
+			var selected2 = document.selection.createRange();
+			var sel = document.selection.createRange();
+			selected2.moveStart ('character', -field.value.length);
+			sel.text = '[' + tag + ']' + selected + '[/' + tag+']';
+			sel.moveStart('character', selected2.text.length + ins.length - selected.length);
 		}
 		//MOZILLA/NETSCAPE/SAFARI support
 		else if (field.selectionStart || field.selectionStart == 0)
@@ -19,14 +24,20 @@ function bbcode_ins(fieldId, tag)
 			field.value = field.value.substring(0, startPos)
 			+ '[' + tag + ']'+ field.value.substring(startPos, endPos) +'[/' + tag+']'
 			+ field.value.substring(endPos, field.value.length);
+			field.setSelectionRange(endPos+tag.length, endPos+tag.length);
 		}
 	} else if (tag == 'url=')
 	{
 		if (document.selection)
 		{
 			field.focus();
-			sel = document.selection.createRange();
-			sel.text = '[' + tag + sel.text + ']'+sel.text+'[/' + tag+']';
+			var selected = document.selection.createRange().text;
+			var ins = '[' + tag + ']' + selected + '[/' + tag +']';
+			var selected2 = document.selection.createRange();
+			var sel = document.selection.createRange();
+			selected2.moveStart ('character', -field.value.length);
+			sel.text = '[' + tag + selected + ']' + selected + '[/' + tag+']';
+			sel.moveStart('character', selected2.text.length + ins.length - selected.length);
 		}
 		//MOZILLA/NETSCAPE/SAFARI support
 		else if (field.selectionStart || field.selectionStart == 0)
@@ -37,14 +48,20 @@ function bbcode_ins(fieldId, tag)
 			field.value = field.value.substring(0, startPos)
 			+ '[' + tag + field.value.substring(startPos, endPos) + ']'+ field.value.substring(startPos, endPos) +'[/' + tag.substring(0, tag.length-1)+']'
 			+ field.value.substring(endPos, field.value.length);
+			field.setSelectionRange(endPos+tag.length+1, endPos+tag.length);
 		}
 	} else if (tag == 'color' || tag == 'size')
 	{
 		if (document.selection)
 		{
 			field.focus();
-			sel = document.selection.createRange();
-			sel.text = '[' + tag + '=]'+sel.text+'[/' + tag+']';
+			var selected = document.selection.createRange().text;
+			var ins = '[' + tag + ']' + selected + '[/' + tag +']';
+			var selected2 = document.selection.createRange();
+			var sel = document.selection.createRange();
+			selected2.moveStart ('character', -field.value.length);
+			sel.text = '[' + tag + selected + ']' + selected + '[/' + tag+']';
+			sel.moveStart('character', selected2.text.length + ins.length - selected.length);
 		}
 		//MOZILLA/NETSCAPE/SAFARI support
 		else if (field.selectionStart || field.selectionStart == 0)
@@ -55,6 +72,7 @@ function bbcode_ins(fieldId, tag)
 			field.value = field.value.substring(0, startPos)
 			+ '[' + tag + '=]'+ field.value.substring(startPos, endPos) +'[/' + tag+']'
 			+ field.value.substring(endPos, field.value.length);
+			field.setSelectionRange(endPos+tag.length+1, endPos+tag.length);
 		}
 	}
 }
